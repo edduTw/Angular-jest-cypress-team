e423064155440f8318f272ab39887f01730a8db8
Gina Franco <gina.franco@thoughtworks.com>
2022-12-07 15:59:00 -0500

    refactor password service: add uppercase, return in another order

--- src/app/data/password.service.ts
+++ src/app/data/password.service.ts
@@ -5,7 +5,7 @@ import { Observable, of } from 'rxjs';
   providedIn: 'root',
 })
 export class PasswordService {
-  password: string = '';
+  
 
   getPassword(
     length: number,
@@ -14,22 +14,25 @@ export class PasswordService {
     symbol: boolean = false
   ): Observable<string> {
     let i: number = 0;
-    this.password = '';
+    let password: string = '';
+    
     while (i < length) {
       if (text && i < length) {
-        this.password += this.getRandomLower();
+        let letter = this.getRandomLower();
+        password += (i % 3 === 0) ? letter.toUpperCase() : letter ;
         i += 1;
       }
       if (numeric && i < length) {
-        this.password += this.getRandomNumber();
+        password += this.getRandomNumber();
         i += 1;
       }
       if (symbol && i < length) {
-        this.password += this.getRandomSymbol();
+        password += this.getRandomSymbol();
         i += 1;
       }
     }
-    return of(this.password);
+    password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
+    return of(password);
   }
 
   getRandomLower(): String {
