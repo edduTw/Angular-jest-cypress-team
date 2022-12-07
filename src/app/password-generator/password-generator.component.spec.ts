6cd704fb3905752c6c103fb5dd98565dcc583f02
Gina Franco <gina.franco@thoughtworks.com>
2022-12-07 16:48:19 -0500

    add scss files and add test

--- src/app/password-generator/password-generator.component.spec.ts
+++ src/app/password-generator/password-generator.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { PasswordGeneratorComponent } from './password-generator.component';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule,By } from '@angular/platform-browser';
 
 describe('PasswordGeneratorComponent', () => {
   let component: PasswordGeneratorComponent;
@@ -23,5 +23,11 @@ describe('PasswordGeneratorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Generate button should be disabled at startup', () => {
+    const submitEl = fixture.debugElement.query(By.css('button'));
+
+    expect(submitEl.nativeElement.disabled).toBeTruthy();
   });
 });
